1c8dd325af1e8456d9d3e331d8c5a6b4fe9a8487
RN-valves <web@rnvalves.com>
2026-09-04 13:09:44 +0530

    Set production BASE_URL to live Render backend

--- blabla/src/service/config.tsx
+++ blabla/src/service/config.tsx
@@ -1,7 +1,3 @@
-import { Platform } from "react-native";
+export const BASE_URL = 'https://busbuddy-ozsn.onrender.com';
 
-export const BASE_URL =
-  Platform.OS === 'android'
-    ? 'http://10.0.2.2:4000'
-    : 'http://localhost:4000';
 
